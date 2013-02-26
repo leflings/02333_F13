73037af844cac7f64e9743fecc9ce8bec219a45a
Flemming Madsen <mail@leflings.dk>
2013-02-26 09:18:44 +0100

    Slight change to accomodate idle thread

--- src/kernel/scheduler.c
+++ src/kernel/scheduler.c
@@ -13,7 +13,14 @@ scheduler_called_from_system_call_handler(const register int schedule)
   if(schedule)
   {
     last_switch = system_time;
-    cpu_private_data.thread_index = thread_queue_dequeue(&ready_queue);
+    if(!thread_queue_is_empty(&ready_queue))
+    {
+      cpu_private_data.thread_index = thread_queue_dequeue(&ready_queue);
+    }
+    else
+    {
+      cpu_private_data.thread_index = -1;
+    }
     return;
   }
   else
@@ -21,8 +28,12 @@ scheduler_called_from_system_call_handler(const register int schedule)
     if(system_time - last_switch > 10)
     {
       last_switch = system_time;
-      thread_queue_enqueue(&ready_queue, cpu_private_data.thread_index);
-      cpu_private_data.thread_index = thread_queue_dequeue(&ready_queue);
+      if(!thread_queue_is_empty(&ready_queue))
+      {
+        /* we only want to switch if there's actually a thread ready */
+        thread_queue_enqueue(&ready_queue, cpu_private_data.thread_index);
+        cpu_private_data.thread_index = thread_queue_dequeue(&ready_queue);
+      }
       return;
     }
   }
