c3a4089d8d8bfeef3ac806ca3de25045ab32e6b7
Flemming Madsen <mail@leflings.dk>
2013-02-26 08:51:36 +0100

    Task B3: Nonpreemptive implemented

--- src/kernel/scheduler.c
+++ src/kernel/scheduler.c
@@ -5,10 +5,27 @@
  */
 
 #include "kernel.h"
+int last_switch = 0;
 
 void
 scheduler_called_from_system_call_handler(const register int schedule)
 {
+  if(schedule)
+  {
+    last_switch = system_time;
+    cpu_private_data.thread_index = thread_queue_dequeue(&ready_queue);
+    return;
+  }
+  else
+  {
+    if(system_time - last_switch > 10)
+    {
+      last_switch = system_time;
+      thread_queue_enqueue(&ready_queue, cpu_private_data.thread_index);
+      cpu_private_data.thread_index = thread_queue_dequeue(&ready_queue);
+      return;
+    }
+  }
 }
 
 void
